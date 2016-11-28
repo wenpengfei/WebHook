abcc3df933475670dc152a07e432e92f848bc40b
wpf <jquery.min.js@gmail.com>
2016-11-28 11:54:11 +0800

    feat: Edit scripts

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ WEB_PATH='/home/fe/HogRider'
 
 echo 'build start....'
 cd $WEB_PATH
+./node_modules/.bin/pm2 delete all
 git clean -f && git pull && yarn install && PORT=3002 npm run prod
+npm start
 echo 'build finish!'
